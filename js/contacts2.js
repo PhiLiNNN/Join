fee3396b57eb8f791694d21a3890a31bae8217ce
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-03-01 02:24:33 +0100

    Desktop vie only css try

--- js/contacts2.js
+++ js/contacts2.js
@@ -7,7 +7,8 @@ function contactsInit() {
     document.body.style.overflow = 'auto';
     const content = document.getElementById("all-contacts-id");
     content.style.paddingTop = '100px';
-    content.style.paddingBottom = '60px';    
+    content.style.paddingBottom = '60px';
+    renderAddContactButtonDesktop();
 }
 
 
@@ -588,3 +589,18 @@ function updateContactMobile(contactId) {
 /* Desktop view */
 
 
+/**
+ * Create add contact button for desktop view
+ */
+function renderAddContactButtonDesktop() {
+  const contentDesktop = document.getElementById("contacts-content-container-desktop-id");
+  const addContactButtonContainerDesktop = document.createElement("div");
+  addContactButtonContainerDesktop.classList.add("addContactButtonContainerDesktop");
+  addContactButtonContainerDesktop.innerHTML = /*html*/ `
+    <button class="addContactButtonDesktop" onclick="addContactShowOverlayDesktop()">Add new contact
+      <span><img class="addContactButtonDesktopImg" src="../assets/img/contacts/addNewContactDesktopButtonImg.svg" alt=""></span></button>    
+    `;    
+  contentDesktop.appendChild(addContactButtonContainerDesktop);  
+  addContactButtonContainerDesktop.addEventListener("click", function () { 
+  });
+}
