c843751179ed867344686c3e5a4519a9421e524d
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2024-03-06 18:44:41 +0100

    function renderAddContactButtonDesktop() moved from contacts_mobile.js to contacts_desktop.js

--- js/contacts_mobile.js
+++ js/contacts_mobile.js
@@ -7,8 +7,7 @@ function contactsInit() {
     document.body.style.overflow = 'auto';
     const content = document.getElementById("all-contacts-id");
     content.style.paddingTop = '100px';
-    content.style.paddingBottom = '60px';
-    renderAddContactButtonDesktop();
+    content.style.paddingBottom = '60px';    
 }
 
 
@@ -591,25 +590,4 @@ function updateContactMobile(contactId) {
   updateCurrentUserInBackend(currentUser);  
   closeContactOverlay();  
   contactsInit();
-}
-
-
-/*-------------------------------------------------------------------------------------------------------------------------------------------------*/
-/* Desktop view */
-
-
-/**
- * Create add contact button for desktop view
- */
-function renderAddContactButtonDesktop() {
-  const contentDesktop = document.getElementById("contacts-content-id");
-  const addContactButtonContainerDesktop = document.createElement("div");
-  addContactButtonContainerDesktop.classList.add("addContactButtonContainerDesktop");
-  addContactButtonContainerDesktop.innerHTML = /*html*/ `
-    <button class="addContactButtonDesktop" onclick="addContactShowOverlayDesktop()">Add new contact
-      <span><img class="addContactButtonDesktopImg" src="../assets/img/contacts/addNewContactDesktopButtonImg.svg" alt=""></span></button>    
-    `;    
-  contentDesktop.appendChild(addContactButtonContainerDesktop);  
-  addContactButtonContainerDesktop.addEventListener("click", function () { 
-  });
 }
