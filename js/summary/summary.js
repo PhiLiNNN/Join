5121c87f3ba440c163fc926414f80e6678987d5b
PhiLiNNN <philipp_wendschuch@web.de>
2024-03-23 17:06:51 +0100

    sarting summary  styling

--- js/summary/summary.js
+++ js/summary/summary.js
@@ -4,50 +4,51 @@ let currentUser;
 function initSummary() {
   isUserLoggedIn = checkUserLogIn();
   if (!isUserLoggedIn) window.location.assign("../error_page.html");
+  currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  console.log("currentUser :>> ", currentUser);
+  toggleVisibility("summary-body-id", true);
+  loadHeaderInitials();
   updateGreeting();
 }
 
 function updateGreeting() {
   const now = new Date();
-  const h = now.getHours();
-  const m = now.getMinutes();
-  const e = document.getElementById("greeting-id");
-  let addHours = 0;
-  let addMinutes = m;
-  if (h <= 4) {
-    // bis 05:00 Uhr
-    e.textContent = "Good night,";
-    addHours = 4 - h;
-  } else if (h <= 10) {
-    // bis 11:00 Uhr
-    e.textContent = "Good morning,";
-    addHours = 10 - h;
-  } else if (h <= 12) {
-    // bis 13:00 Uhr
-    e.textContent = "Good noon,";
-    addHours = 12 - h;
-  } else if (h <= 17) {
-    // bis 18:00 Uhr
-    e.textContent = "Good afternoon,";
-    addHours = 17 - h;
-  } else {
-    // bis 04:00 Uhr
-    e.innerHTML = "Good evening,";
-    addHours = 23 - h;
-  }
-  const waitTime = addHours * 60 * 60 * 1000 + addMinutes * 60 * 1000;
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const element = document.getElementById("greeting-id");
+  let {addHours, greeting} = checkDayTime(hours);
+  element.innerHTML = templateGreetingsHTML(greeting, currentUser.userName);
+  const waitTime = addHours * 60 * 60 * 1000 + minutes * 60 * 1000;
   setTimeout(updateGreeting, waitTime);
-  currentUser = JSON.parse(localStorage.getItem("currentUser"));
-  toggleVisibility("summary-body-id", true);
-  loadHeaderInitials();
 }
 
-async function requestToBackend() {
-  try {
-    const result = await getItem("currentUser");
-    return JSON.parse(result) || [];
-  } catch (e) {
-    console.error("Loading error:", e);
-    return [];
+function checkDayTime(hours) {
+  let addHours = 0;
+  let greeting;
+  if (hours <= 4) {
+    greeting = "Good night"; // bis 05:00 Uhr
+    addHours = 4 - hours;
+  } else if (hours <= 10) {
+    greeting = "Good morning"; // bis 11:00 Uhr
+    addHours = 10 - hours;
+  } else if (hours <= 12) {
+    greeting = "Good noon"; // bis 13:00 Uhr
+    addHours = 12 - hours;
+  } else if (hours <= 17) {
+    greeting = "Good afternoon"; // bis 18:00 Uhr
+    addHours = 17 - hours;
+  } else {
+    greeting = "Good evening"; // bis 04:00 Uhr
+    addHours = 23 - hours;
   }
+  return {addHours, greeting};
+}
+
+function templateGreetingsHTML(greeting, user) {
+  return /*html*/ `
+    <div class="greetings-content" >
+      <span>${greeting}${user !== "guest user" ? "," : ""}</span>
+      <span class="user-highlight">${user !== "guest user" ? `<span>${user}</span>` : ""}</span>
+    </div>
+  `;
 }
